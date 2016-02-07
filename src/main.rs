1118baf0111e5ffa23ee1e3e7701a83320b1c556
Kamal Marhubi <kamal@marhubi.com>
2016-02-07 02:10:04 -0500

    Get multirust::Cfg set up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+#[macro_use(shared_ntfy)]
+extern crate rust_install;
+
 extern crate multirust;
 
+use multirust::{Cfg, Result, Notification};
+
+// Copied from multirust-rs.
+fn set_globals() -> Result<Cfg> {
+    // Base config
+    let verbose = false;
+    Cfg::from_env(shared_ntfy!(move |n: Notification| {
+        use multirust::notify::NotificationLevel::*;
+        match n.level() {
+            Verbose => {
+                if verbose {
+                    println!("{}", n);
+                }
+            }
+            _ => {
+                println!("{}", n);
+            }
+        }
+    }))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = set_globals().expect("set_globals");
+    println!("{:?}", cfg.multirust_dir);
 }
