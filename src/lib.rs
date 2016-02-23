deb94ff9bdd8f61f9d413c8abedbbbf1cb49323c
Kamal Marhubi <kamal@marhubi.com>
2016-02-23 10:14:35 -0500

    Introduce Nightly newtype over NaiveDate
    Moves some parsing noise out of the main function.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ extern crate clap;
 extern crate multirust;
 extern crate rust_install;
 
-use std::error;
+use std::{error, fmt, str};
 
 use chrono::NaiveDate;
+use rust_install::dist::ToolchainDesc;
 
 pub const NIGHTLY: &'static str = "nightly";
 
@@ -17,6 +18,42 @@ pub use bisect::least_satisfying;
 
 pub mod cli;
 
-pub fn nightly(date: NaiveDate) -> String {
-    format!("{}-{}", NIGHTLY, date)
+#[derive(Clone, Copy, Debug)]
+pub struct Nightly {
+    pub date: NaiveDate,
+}
+
+impl Nightly {
+    pub fn to_toolchain_desc(&self) -> ToolchainDesc {
+        ToolchainDesc {
+            date: Some(self.date.to_string()),
+            channel: String::from(NIGHTLY),
+            arch: None,
+            os: None,
+            env: None,
+        }
+    }
+}
+
+impl From<NaiveDate> for Nightly {
+    fn from(date: NaiveDate) -> Nightly {
+        Nightly { date: date }
+    }
+}
+
+impl str::FromStr for Nightly {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Nightly> {
+        let desc = try!(ToolchainDesc::from_str(s).ok_or("invalid toolchain name"));
+        if desc.channel != NIGHTLY || desc.date.is_none() {
+            return Err(Error::from("not a dated nightly"));
+        }
+        Ok(Nightly { date: try!(desc.date.unwrap().parse()) })
+    }
+}
+
+impl fmt::Display for Nightly {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}-{}", NIGHTLY, self.date)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,26 @@ use std::process;
 
 use chrono::NaiveDate;
 use hyper::client::Client;
-use rust_install::dist::ToolchainDesc;
 
 use rust_bisect::cli;
-use rust_bisect::{Result, least_satisfying, nightly};
+use rust_bisect::{Nightly, Result, least_satisfying};
 
 fn list_available_nightlies(dist_root: &str,
                             from: NaiveDate,
                             to: NaiveDate)
-                            -> Result<Vec<NaiveDate>> {
+                            -> Result<Vec<Nightly>> {
     assert!(from < to, "`from` must be less than `to`");
     println!("finding available nightlies between {} and {}", from, to);
     let client = Client::new();
     let mut nightlies = Vec::with_capacity((to - from).num_days() as usize);
     let mut date = from;
     while date < to {
-        let desc = ToolchainDesc::from_str(&nightly(date)).expect("should always parse");
-        let resp = try!(client.head(&desc.manifest_url(dist_root)).send());
+        let nightly = Nightly::from(date);
+        let manifest_url = nightly.to_toolchain_desc().manifest_url(dist_root);
+        let resp = try!(client.head(&manifest_url).send());
         // TODO: ensure failures are 404
         if resp.status.is_success() {
-            nightlies.push(date);
+            nightlies.push(nightly);
         }
         date = date.succ();
     }
@@ -42,18 +42,14 @@ fn run_rust_bisect() -> Result<i32> {
 
     let cfg = try!(multirust::Cfg::from_env(rust_install::notify::SharedNotifyHandler::none()));
 
-    let good = matches.value_of("good").expect("clap didn't respect required arg `good`");
-    let good = ToolchainDesc::from_str(good).expect("clap validator misbehaved for `good`");
-
-    let bad = matches.value_of("bad").expect("clap didn't respect required arg `bad`");
-    let bad = ToolchainDesc::from_str(bad).expect("clap validator misbehaved for `bad`");
-
-    let good_date: NaiveDate = try!(good.date
-                                        .expect("clap validator misbehaved for `good`")
-                                        .parse());
-    let bad_date: NaiveDate = try!(bad.date
-                                      .expect("clap validator misbehaved for `bad`")
-                                      .parse());
+    let good: Nightly = matches.value_of("good")
+                               .expect("clap didn't respect required arg `good`")
+                               .parse()
+                               .expect("clap validator misbehaved for `good`");
+    let bad: Nightly = matches.value_of("bad")
+                              .expect("clap didn't respect required arg `bad`")
+                              .parse()
+                              .expect("clap validator misbehaved for `bad`");
 
     let cmd = matches.value_of_os("COMMAND")
                      .expect("clap didn't respect required arg `COMMAND`");
@@ -61,16 +57,16 @@ fn run_rust_bisect() -> Result<i32> {
                               .map(|args| args.collect())
                               .unwrap_or(Vec::new());
 
-    let nightlies = try!(list_available_nightlies(&*cfg.dist_root_url, good_date, bad_date));
+    let nightlies = try!(list_available_nightlies(&*cfg.dist_root_url, good.date, bad.date));
     println!("bisecting across {} nightlies (about {} steps)",
              nightlies.len(),
              nightlies.len().next_power_of_two().trailing_zeros());
 
-    let idx = least_satisfying(&nightlies[..], |date| {
-        let version = nightly(*date);
-        println!("testing with {}", version);
+    let idx = least_satisfying(&nightlies[..], |nightly| {
+        println!("testing with {}", nightly);
 
-        let toolchain = cfg.get_toolchain(&version, false).expect("could not get toolchain");
+        let toolchain = cfg.get_toolchain(&nightly.to_string(), false)
+                           .expect("could not get toolchain");
         toolchain.install_from_dist_if_not_installed().expect("could not install toolchain");
 
         let mut cmd = toolchain.create_command(cmd).expect("could not create command");
@@ -83,11 +79,11 @@ fn run_rust_bisect() -> Result<i32> {
                  } else {
                      "failed"
                  },
-                 version);
+                 nightly);
         !res
     });
 
-    println!("{} is the first failing nightly", nightly(nightlies[idx]));
+    println!("{} is the first failing nightly", nightlies[idx]);
     Ok(libc::EXIT_SUCCESS)
 }
 
