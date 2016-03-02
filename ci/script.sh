3f2dbe0574ef9139817a5172c3134c62c2aa5f91
Kamal Marhubi <kamal@marhubi.com>
2016-03-02 14:28:01 -0500

    ci: Add binary releases adapted from rust-everywhere

---
+++ ci/script.sh
@@ -0,0 +1,16 @@
+# `script` phase: you usually build, test and generate docs in this phase
+
+set -ex
+
+# PROTIP Always pass `--target $TARGET` to cargo commands, this makes cargo output build artifacts
+# to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
+# `before_deploy`/packaging phase
+
+cargo build --target $TARGET --verbose
+cargo run --target $TARGET
+cargo test --target $TARGET
+
+cargo build --target $TARGET --release
+
+# sanity check the file type
+file target/$TARGET/release/rust-bisect
