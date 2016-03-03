b4bcbc64b4ba98deb5228a989dabd39fea8da815
Kamal Marhubi <kamal@marhubi.com>
2016-03-02 20:56:57 -0500

    ci: Clone multirust-rs to avoid long submodule clone
    We *really* don't need those binaries.

--- ci/script.sh
+++ ci/script.sh
@@ -15,6 +15,35 @@ doctest = false
 EOF
 fi
 
+# Speed up getting multirust-rs by cloning the repo ourselves and not fetching the binaries submodule.
+echo Cloning multirust
+tempdir=$(mktemp -d /tmp/multirust-clone.XXXXXX)
+trap "rm -rf $tempdir" EXIT
+old_cwd=$(pwd)
+cd $tempdir
+git clone --single-branch --branch new https://github.com/Diggsey/multirust-rs.git
+cd multirust-rs
+git checkout c350ddb447b6bd7e431c1a8af796bb5b345b8e8d
+cd $old_cwd
+
+multirust=$tempdir/multirust-rs
+rust_install=$multirust/rust-install
+
+echo Overriding source for multirust and rust-install
+patch Cargo.toml <<EOF
+33,34c33
+< git = "https://github.com/Diggsey/multirust-rs.git"
+< rev = "c350ddb447b6bd7e431c1a8af796bb5b345b8e8d"
+---
+> path = "$multirust"
+37,39c36
+< git = "https://github.com/Diggsey/multirust-rs.git"
+< rev = "c350ddb447b6bd7e431c1a8af796bb5b345b8e8d"
+< path = "rust-install"
+---
+> path = "$rust_install"
+EOF
+
 cargo build --target $TARGET --verbose
 cargo run --target $TARGET --example rust-issue-30123
 cargo test --target $TARGET
