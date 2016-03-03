869d556306ce1b02a11fff67bffe1726391526a3
Kamal Marhubi <kamal@marhubi.com>
2016-03-02 23:31:06 -0500

    ci: Change how multirust clone override is done

--- ci/script.sh
+++ ci/script.sh
@@ -6,6 +6,16 @@ set -ex
 # to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
 # `before_deploy`/packaging phase
 
+# Speed up getting multirust-rs by cloning the repo ourselves and not fetching the binaries submodule.
+echo Cloning multirust
+git clone --single-branch --branch new https://github.com/Diggsey/multirust-rs.git
+cd multirust-rs
+git checkout c350ddb447b6bd7e431c1a8af796bb5b345b8e8d
+cd ..
+
+echo Replacing Cargo.toml to override multirust path
+cp ci/Cargo.toml.multirust-override ./Cargo.toml
+
 # Disable doctests when cross-compiling as there appears to be an issue.
 host=$(rustc -vV | grep host | awk '{print $2}')
 if [ $TARGET != $host ]; then
@@ -14,35 +24,6 @@ if [ $TARGET != $host ]; then
 doctest = false
 EOF
 fi
-
-# Speed up getting multirust-rs by cloning the repo ourselves and not fetching the binaries submodule.
-echo Cloning multirust
-tempdir=$(mktemp -d /tmp/multirust-clone.XXXXXX)
-trap "rm -rf $tempdir" EXIT
-old_cwd=$(pwd)
-cd $tempdir
-git clone --single-branch --branch new https://github.com/Diggsey/multirust-rs.git
-cd multirust-rs
-git checkout c350ddb447b6bd7e431c1a8af796bb5b345b8e8d
-cd $old_cwd
-
-multirust=$tempdir/multirust-rs
-rust_install=$multirust/rust-install
-
-echo Overriding source for multirust and rust-install
-patch Cargo.toml <<EOF
-33,34c33
-< git = "https://github.com/Diggsey/multirust-rs.git"
-< rev = "c350ddb447b6bd7e431c1a8af796bb5b345b8e8d"
----
-> path = "$multirust"
-37,39c36
-< git = "https://github.com/Diggsey/multirust-rs.git"
-< rev = "c350ddb447b6bd7e431c1a8af796bb5b345b8e8d"
-< path = "rust-install"
----
-> path = "$rust_install"
-EOF
 
 cargo build --target $TARGET --verbose
 cargo run --target $TARGET --example rust-issue-30123
