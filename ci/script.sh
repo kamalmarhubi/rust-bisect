227860a757bc6fd10a23056efd270e5cacd47746
Kamal Marhubi <kamal@marhubi.com>
2016-03-02 20:26:18 -0500

    ci: Disable doctests when cross-compiling
    Seems to give the wrong build of crates in `extern crate`.

--- ci/script.sh
+++ ci/script.sh
@@ -6,10 +6,18 @@ set -ex
 # to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
 # `before_deploy`/packaging phase
 
+# Disable doctests when cross-compiling as there appears to be an issue.
+host=$(rustc -vV | grep host | awk '{print $2}')
+if [ $TARGET != $host ]; then
+  cat >> Cargo.toml <<EOF
+[lib]
+doctest = false
+EOF
+fi
+
 cargo build --target $TARGET --verbose
 cargo run --target $TARGET --example rust-issue-30123
 cargo test --target $TARGET
-
 cargo build --target $TARGET --release
 
 # sanity check the file type
